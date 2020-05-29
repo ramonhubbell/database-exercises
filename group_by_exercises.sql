c37c15c51b69c922fe78e765c2cdb50a1c4115f8
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-29 14:34:24 -0500

    finished join exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,12 +2,14 @@ use employees;
 
 SELECT DISTINCT title FROM titles;
 
+select title, count(*) from titles group by title;
+
 # Find your query for employees whose last names start and end with
 # 'E'. Update the query to find just the unique last names that start
 # and end with 'E' using GROUP BY.
 select last_name, count(last_name) as 'last_name repeats'
 from employees where last_name like 'e%' and last_name like '%e'
-group by last_name
+group by last_name;
 
 # Update your previous query to now find unique combinations of first and
 # last name where the last name starts and ends with 'E'. You should get
@@ -20,7 +22,8 @@ group by first_name, last_name;
 # Find the unique last names with a 'q' but not 'qu'.
 select last_name from employees
 where last_name like '%q%' and last_name not like '%qu%'
-group by last_name;
+group by last_name
+order by last_name;
 
 # Add a COUNT() to your results and use ORDER BY to make it easier to find
 # employees whose unusual name is shared with others.
---
+++ join_exercises.sql
@@ -0,0 +1,46 @@
+use employees;
+show tables;
+select * from dept_emp;
+select * from dept_manager;
+
+select dept_name as 'Department', concat(first_name, ' ', last_name) as 'Department Manager'
+from departments as d
+join dept_manager as dm
+on dm.dept_no = d.dept_no
+join employees as e
+on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+order by dept_name;
+
+# Find the name of all departments currently managed by women.
+select dept_name as 'Department', concat(first_name, ' ', last_name) as 'Department Manager', gender
+from departments as d
+         join dept_manager as dm
+              on dm.dept_no = d.dept_no
+         join employees as e
+              on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' and e.gender = 'F'
+order by dept_name;
+
+# Find the current titles of employees currently working in the Customer Service department.
+
+select titles.title as 'Title', count( titles.emp_no) as 'Count' from titles
+join dept_emp as de on titles.emp_no = de.emp_no
+join departments as d on de.dept_no = d.dept_no
+where titles.to_date  > curdate()
+   and d.dept_name = 'Customer Service'
+  and de.to_date > curdate()
+group by titles.title;
+
+# Find the current salary of all current managers.
+select dept_name as 'Department', concat(first_name, ' ', last_name) as 'Department Manager',
+       s.salary as Salary
+from departments as d
+         join dept_manager as dm
+              on dm.dept_no = d.dept_no
+         join employees as e
+              on dm.emp_no = e.emp_no
+        join salaries as s
+            on e.emp_no = s.emp_no
+WHERE dm.to_date > curdate() and s.to_date > curdate()
+order by dept_name;
